1a70df8705c39fc48e830944c410a8356f89283b
CatalystMonish <monishmeher@gmail.com>
2023-08-18 01:29:00 +0530

    structured

--- src/components/DomainCard.jsx
+++ src/components/DomainCard.jsx
@@ -5,7 +5,7 @@ import pythonImg from '../Images/python.jpg'
 
 function DomainCard() {
   return (
-    <div className='flex flex-col px-3 py-4 w-117 h-142 rounded-xl shadow-md m-3 space-y-1 bg-white justify-center items-center'>
+    <div className='flex flex-col px-3 py-4 w-117 h-142 rounded-xl shadow-md m-3 space-y-1 bg-white justify-center items-center font-poppins'>
       <img className='w-2/3 h-auto mx-auto' src={pythonImg} alt='Python Logo' />
       <p className='text-center font-medium text-xs'>Python</p>
       <p className='text-center text-[#646464] text-xs'>10+ Projects</p>
